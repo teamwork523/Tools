0401a492ea90b71f52af67e0ae3c74eccdd0c3dd
Snooperlock <teamworkucla@hotmail.com>
2013-09-19 22:39:21 -0400

    Update file diff computation

--- common/compute_folder_diff.sh
+++ common/compute_folder_diff.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# TODO: make it automatic recursively print for all subfolders
+
+if [ "$1" == "-h" ]; then
+    echo "./compute_folder_diff.sh folder1 folder2"
+    exit 1
+fi
+
 folder1=$1
 folder2=$2
 
@@ -7,12 +14,16 @@ folder1_filenames=$(ls $folder1 | tee temp1.txt)
 folder2_filenames=$(ls $folder2 | tee temp2.txt)
 
 # compare file difference
+echo "***********************"
+echo "File items diff:"
+echo "***********************"
 diff -u temp1.txt temp2.txt
 
 for i in $folder1_filenames; do
-    echo "*********************"
-    echo "File difference at $i"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>"
+    echo "File content diff at $i"
     diff -u $folder1$i $folder2$i
+    echo "<<<<<<<<<<<<<<<<<<<<<<<<"
 done
 
 rm temp1.txt temp2.txt 2> /dev/null
