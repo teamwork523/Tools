3a1a0d1f680c872db2b5579cec5bbaf75ceee75c
teamwork523 <teamworkucla@hotmail.com>
2014-12-10 01:21:11 -0800

    Check username change

--- common/whatIsMyIpAddr.sh
+++ common/whatIsMyIpAddr.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-wget -qO- http://ipecho.net/plain ; echo
 # Alternative solution: curl -s checkip.dyndns.org|sed -e 's/.*Current IP Address: //' -e 's/<.*$//'
+wget -qO- http://ipecho.net/plain ; echo
